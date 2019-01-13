3b8c1872231606a1354a6e746b428360a67f9384
Antonio santos <tozeps@gmail.com>
2019-01-13 19:41:29 +0000

    some

--- config/db.js
+++ config/db.js
@@ -3,11 +3,10 @@ let password = 'adagui78';
 let dbhost = 'ds155714.mlab.com';
 let port = '55714';
 let dbname = 'estacao';
-mongodb://<dbuser>:<dbpassword>@ds155714.mlab.com:55714/estacao
 
 module.exports = {
-  "url": "mongodb://heroku_7zl5p8dh:kd1f60os6pgal0dl8luo7jbf4f@ds255794.mlab.com:55794/heroku_7zl5p8dh",
-  "url_alternative": `mongodb://${username}:${password}@${dbhost}:${port}/${dbname}`,
+  "url": `mongodb://${username}:${password}@${dbhost}:${port}/${dbname}`,
+  "url_alternative": "mongodb://localhost:27017/estacao",
   "db": dbname,
   "collection": {
     "user": "user",
